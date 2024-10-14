d85c2345e9095324523273b94218dbe57bf84be0
Bolo Lai <cantonbolo@gmail.com>
2024-10-15 01:44:35 +0800

    颜色过渡

--- press/components/Filter/FilterBar.vue
+++ press/components/Filter/FilterBar.vue
@@ -27,49 +27,49 @@ const filterList = computed({
         <button @click="toggleFilter('windows')"
             class="inline-block px-4 py-2 text-sm font-medium text-gray-700 dark:text-gray-400 hover:bg-gray-50 dark:hover:bg-gray-700 focus:relative">
             Windows
-            <i
+            <i class="transition-colors duration-150 ease-in-out"
                 :class="[(filterList || []).includes('windows') ? 'i-carbon-checkbox-checked-filled text-green' : '', 'i-carbon-checkbox']"></i>
         </button>
 
         <button @click="toggleFilter('mac')"
             class="inline-block px-4 py-2 text-sm font-medium text-gray-700 dark:text-gray-400 hover:bg-gray-50 dark:hover:bg-gray-700 focus:relative">
             Mac
-            <i
+            <i class="transition-colors duration-150 ease-in-out"
                 :class="[(filterList || []).includes('mac') ? 'i-carbon-checkbox-checked-filled text-green' : '', 'i-carbon-checkbox']"></i>
         </button>
 
         <button @click="toggleFilter('linux')"
             class="inline-block px-4 py-2 text-sm font-medium text-gray-700 dark:text-gray-400 hover:bg-gray-50 dark:hover:bg-gray-700 focus:relative">
             Linux
-            <i
+            <i class="transition-colors duration-150 ease-in-out"
                 :class="[(filterList || []).includes('linux') ? 'i-carbon-checkbox-checked-filled text-green' : '', 'i-carbon-checkbox']"></i>
         </button>
 
         <button @click="toggleFilter('android')"
             class="inline-block px-4 py-2 text-sm font-medium text-gray-700 dark:text-gray-400 hover:bg-gray-50 dark:hover:bg-gray-700 focus:relative">
             Android
-            <i
+            <i class="transition-colors duration-150 ease-in-out"
                 :class="[(filterList || []).includes('android') ? 'i-carbon-checkbox-checked-filled text-green' : '', 'i-carbon-checkbox']"></i>
         </button>
 
         <button @click="toggleFilter('ios')"
             class="inline-block px-4 py-2 text-sm font-medium text-gray-700 dark:text-gray-400 hover:bg-gray-50 dark:hover:bg-gray-700 focus:relative">
             iOS
-            <i
+            <i class="transition-colors duration-150 ease-in-out"
                 :class="[(filterList || []).includes('ios') ? 'i-carbon-checkbox-checked-filled text-green' : '', 'i-carbon-checkbox']"></i>
         </button>
 
         <button @click="toggleFilter('appletv')"
             class="inline-block px-4 py-2 text-sm font-medium text-gray-700 dark:text-gray-400 hover:bg-gray-50 dark:hover:bg-gray-700 focus:relative">
             AppleTV
-            <i
+            <i class="transition-colors duration-150 ease-in-out"
                 :class="[(filterList || []).includes('appletv') ? 'i-carbon-checkbox-checked-filled text-green' : '', 'i-carbon-checkbox']"></i>
         </button>
 
         <button @click="toggleFilter('router')"
             class="inline-block px-4 py-2 text-sm font-medium text-gray-700 dark:text-gray-400 hover:bg-gray-50 dark:hover:bg-gray-700 focus:relative">
             路由器
-            <i
+            <i class="transition-colors duration-150 ease-in-out"
                 :class="[(filterList || []).includes('router') ? 'i-carbon-checkbox-checked-filled text-green' : '', 'i-carbon-checkbox']"></i>
         </button>
     </span>
