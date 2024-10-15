541a75c2f3c0e3bb29ff465b1e925dbb13238cfa
Bolo Lai <cantonbolo@gmail.com>
2024-10-15 23:12:01 +0800

    合并dev

--- press/.vitepress/theme/index.ts
+++ press/.vitepress/theme/index.ts
@@ -15,7 +15,7 @@ export default {
   enhanceApp({ app, router, siteData }) {
     app.component(
       "Embed",
-      defineAsyncComponent(() => import("../../components/Embeds/Embed.vue"))
+      defineAsyncComponent(() => import("../../components/Embed.vue"))
     );
     app.component(
       "AppStore",
