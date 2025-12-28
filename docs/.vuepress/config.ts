1539d8db245a1a670a837f506b0bdf54b21263d5
Bolo Lai <cantonbolo@gmail.com>
2025-12-28 16:46:05 +0800

    修改配置参数

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,16 +1,18 @@
 import { viteBundler } from '@vuepress/bundler-vite'
 import { defineUserConfig } from 'vuepress'
 import { plumeTheme } from 'vuepress-theme-plume'
-import plumeConfig from './plume.config'
+import { getPlumeConfig } from './plume.config'
+import { ThemeOptions } from 'vuepress-theme-plume'
+
+const plumeConfig = await getPlumeConfig()
+console.log(plumeConfig)
 
 export default defineUserConfig({
   // 必须设置默认语言
   lang: 'zh-CN',
   title: 'YaNet',
   description: 'Yet another Internet.',
-  head: [
-    ['link', { rel: 'icon', type: 'image/png', href: '/logo.png' }],
-  ],
-  theme: plumeTheme(plumeConfig),
+  head: [['link', { rel: 'icon', type: 'image/png', href: '/logo.png' }]],
+  theme: plumeTheme(plumeConfig as ThemeOptions),
   bundler: viteBundler(),
 })
--- docs/.vuepress/plume.config.ts
+++ docs/.vuepress/plume.config.ts
@@ -1,22 +1,49 @@
 import { defineThemeConfig } from 'vuepress-theme-plume'
+import axios from 'axios'
+import { writeFileSync } from 'node:fs'
+import { join } from 'node:path'
 
-export default defineThemeConfig({
-  logo: '/logo@0.1x.png',
-  profile: {
-    name: 'YaNet',
-  },
-  collections: [
-    {
-      type: 'post',
-      dir: 'blog',
-      title: '博客',
+async function getGithubProfile() {
+  return await axios
+    .get('https://api.github.com/user/146246')
+    .then((res) => res.data)
+}
+
+export async function getPlumeConfig() {
+  const profile = await getGithubProfile()
+  const avatarResponse = await axios
+    .get(profile.avatar_url, { responseType: 'arraybuffer' })
+    .then((res) => res.data)
+  const publicDir = join(process.cwd(), 'docs', '.vuepress', 'public')
+  writeFileSync(join(publicDir, 'avatar.png'), avatarResponse)
+
+  return {
+    logo: '/logo@0.1x.png',
+    profile: {
+      avatar: `/avatar.png`,
+      name: profile.name,
+      description: profile.bio,
+      circle: true,
     },
-    {
-      type: 'doc',
-      dir: 'github',
-      linkPrefix: 'github',
-      title: '逛逛Github',
-    },
-  ],
-  // 更多配置...
-})
+    social: [
+      {
+        icon: 'github',
+        link: profile.html_url,
+      },
+    ],
+    collections: [
+      {
+        type: 'post',
+        dir: 'blog',
+        linkPrefix: 'blog',
+        title: '博客',
+      },
+      {
+        type: 'doc',
+        dir: 'github',
+        linkPrefix: 'github',
+        title: '逛逛Github',
+      },
+    ],
+  }
+}
